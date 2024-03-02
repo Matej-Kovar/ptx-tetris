b24853ff0325622f44f65f9720b2b0a33616b933
matej-kovar <146720349+Matej-Kovar@users.noreply.github.com>
2024-03-02 12:32:14 +0100

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://matej-kovar.github.io/ptx-tetris/](https://matej-kovar.github.io/ptx-tetris/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/matej-kovar/ptx-tetris** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/matej-kovar/ptx-tetris** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,93 @@
+type Xy = {
+    x: number
+    y: number
+}
+type Row = Array<boolean>;
+type Display = Array<Row>;
+const displayData: Display = [
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+]
+const dot: Xy = {
+    x: 2,
+    y: 0
+}
+let fallTimer:number = 500;
+let gameOver:boolean = false;
+function render(display: Display):void{
+    for(let y=0; y < 5; y++){
+        for (let x = 0; x < 5; x++) {
+            led.unplot(y, x)
+        }
+    }
+    for (let y = 0; y < 5; y++) {
+        for (let x = 0; x < 5; x++) {
+            if(display[y][x]){
+                led.plot(x, y)
+            }
+        }
+    }
+}
+function scoreDropEnd(coord: Xy, display: Display){
+    if (display[coord.y].every((test) => {return test === true})){
+        for (let x = 0; x < 5; x++) {
+                display[coord.y][x] = false;
+        }
+    }
+    coord.y = 0;
+    coord.x = 2;
+    if(display[coord.y + 1][coord.x]){
+        gameOver = true;
+    }
+    display[coord.y][coord.x] = true;
+}
+function fall(coord: Xy, display: Display):void{
+    display[coord.y][coord.x] = false;
+    coord.y += 1;
+    display[coord.y][coord.x] = true;
+}
+function moveLeft(coord: Xy, display: Display): void {
+    if (coord.x - 1 >= 0 && !display[coord.y][coord.x - 1]) {
+        display[coord.y][coord.x] = false;
+        coord.x -= 1;
+        display[coord.y][coord.x] = true;
+    }
+}
+function moveRight(coord: Xy, display: Display): void {
+    if(coord.x + 1 <= 4 && !display[coord.y][coord.x + 1]){
+        display[coord.y][coord.x] = false;
+        coord.x += 1;
+        display[coord.y][coord.x] = true;
+    }
+}
+ 
+loops.everyInterval(80, function () {
+    if (!gameOver){
+         render(displayData)
+        if (input.buttonIsPressed(Button.A)) {
+                moveLeft(dot, displayData)
+        }
+        if (input.buttonIsPressed(Button.B)) {
+                moveRight(dot, displayData)
+            }
+    }else{
+        basic.showIcon(IconNames.Sad)
+        basic.pause(1000);
+    }
+
+})
+loops.everyInterval(fallTimer, function () {
+    if (dot.y === 4) {
+        scoreDropEnd(dot, displayData)
+    }
+    else if (displayData[dot.y + 1][dot.x]) {
+        scoreDropEnd(dot, displayData)
+    }
+    else {
+        fall(dot, displayData)
+    }
+    fallTimer = fallTimer - 20;
+    })
